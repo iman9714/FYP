2bf76eebbaf7885f63554716da3fa0837c5de94a
muhammad iman <iman9714@gmail.com>
2019-11-07 22:23:36 +0800

    init commit

--- EMS/urls.py
+++ EMS/urls.py
@@ -3,5 +3,5 @@ from .views import HomePageView,LoginPageView
 
 urlpatterns = [
     path('login/',LoginPageView.as_view(), name='login'),
-    path('',HomePageView.as_view(), name='home'),
+    path('home/',HomePageView.as_view(), name='home'),
 ]
